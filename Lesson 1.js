59c71eb949f3b35dd63f12dbe84382e01720f1fd
Dmytro Myroshnychenko <103591499+1amak@users.noreply.github.com>
2022-05-14 00:49:59 +0300

    A JavaScript Boolean and Conditional statements
    A JavaScript Boolean and Conditional statements (if else)

---
+++ Lesson 1.js
@@ -0,0 +1,35 @@
+let yolochka4 = 4;
+let yolochka6 = 6;
+let yolochka_name = "Zelyonaya"
+
+let result = yolochka4 + yolochka6
+let result_2 = yolochka6 + yolochka_name
+
+
+// console.log(result)
+// console.log(result_2)
+
+// let t = true
+// let f = false
+
+let age = 18;
+
+
+if (typeof(age) == "number") {
+    if (age >= 18 && age < 60) {
+    console.log("Full content")
+}
+    else if (age < 18) {
+        console.log("BAN")
+    }
+    else if (age >= 60) {
+       
+        console.log("Heart attack")
+    } 
+    else {
+        console.log("Good luck")
+    }
+
+}
+
+
